86564f978ddf5f8c1c18b8ab197da5071067640b
Charles <cmersereau@waysuninc.com>
2020-08-27 14:24:33 -0400

    adding android publishing variants

--- app/src/main/java/com/waysuninc/waysunflipbackenddemo/ui/user/UserViewModel.kt
+++ app/src/main/java/com/waysuninc/waysunflipbackenddemo/ui/user/UserViewModel.kt
@@ -6,13 +6,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.waysuninc.flipbackendapi.FlipRepo
 import com.waysuninc.flipbackendapi.api.PokeApi
+import com.waysuninc.flipbackendapi.api.UserApi
 import com.waysuninc.flipbackendapi.entities.User
+import com.waysuninc.kotlin_lib.HelloWorld
 import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
 
-    val api = PokeApi()
-    val repo = FlipRepo(api)
+    val api: UserApi = PokeApi()
+    val repo: FlipRepo = FlipRepo(api)
+    val helloWorld: String = HelloWorld().getHelloWorld()
 
     private val _user = MutableLiveData<User>().apply {
         viewModelScope.launch {
---
+++ kotlin-lib/src/main/java/com/waysuninc/kotlin_lib/HelloWorld.kt
@@ -0,0 +1,7 @@
+package com.waysuninc.kotlin_lib
+
+class HelloWorld {
+    fun getHelloWorld(): String {
+        return "Hello World"
+    }
+}
